39a5a79e915da857301d9bae499c8270f1c492f0
Nicola Porta <nicola.porta@Air-di-SPXUSER.homenet.telecomitalia.it>
2021-06-23 14:40:35 +0200

    add new route testNicola

--- app/containers/TestNicola/saga.js
+++ app/containers/TestNicola/saga.js
@@ -6,7 +6,7 @@ import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { changeToDo } from 'actions';
 
 import request from 'utils/request';
-import { makeSelectUsername } from 'containers/HomePage/selectors';
+import { makeSelectId } from 'containers/HomePage/selectors';
 
 /**
  * Github repos request/response handler
@@ -28,10 +28,10 @@ export function* getToDo() {
 /**
  * Root saga manages watcher lifecycle
  */
-export default function* githubData() {
+export default function* getToDoData() {
   // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  yield takeLatest(LOAD_REPOS, getRepos);
+  yield takeLatest('TO_DO', getToDo);
 }
