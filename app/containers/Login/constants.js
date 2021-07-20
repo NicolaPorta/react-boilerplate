26cdb315ef9f222c9ff788b0346c8990c69c8b51
Nicola <nicola.porta00@gmail.com>
2021-07-20 17:17:23 +0200

    Constants scope

--- app/containers/Login/constants.js
+++ app/containers/Login/constants.js
@@ -1,6 +1,7 @@
-export const LOGIN_ACTION = 'LOGIN_ACTION';
-export const LOGIN_SUCCESS_ACTION = 'LOGIN_SUCCESS_ACTION';
-export const LOGIN_ERROR_ACTION = 'LOGIN_ERROR_ACTION';
-export const USER_AUTH_VALIDATION = 'USER_AUTH_VALIDATION';
-export const USER_LOGOUT = 'USER_LOGOUT';
-export const USER_LOGOUT_SUCCESS = 'USER_LOGOUT_SUCCESS';
+const root = 'boilerplate/Login/';
+export const LOGIN_ACTION = `${root}LOGIN_ACTION`;
+export const LOGIN_SUCCESS_ACTION = `${root}LOGIN_SUCCESS_ACTION`;
+export const LOGIN_ERROR_ACTION = `${root}LOGIN_ERROR_ACTION`;
+export const USER_AUTH_VALIDATION = `${root}USER_AUTH_VALIDATION`;
+export const USER_LOGOUT = `${root}USER_LOGOUT`;
+export const USER_LOGOUT_SUCCESS = `${root}USER_LOGOUT_SUCCESS`;
--- app/containers/ToDos/constants.js
+++ app/containers/ToDos/constants.js
@@ -3,14 +3,11 @@
  * Call TODO constants
  *
  */
-export const CALL_TODO_LIST = 'CALL_TODO_LIST';
-export const CALL_TODO_SUCCESS = 'CALL_TODO_SUCCESS';
-export const CALL_TODO_REJECTED = 'CALL_TODO_REJECTED';
-export const DELETE_TODO = 'DELETE_TODO';
-export const DELETE_TODO_ERROR = 'DELETE_TODO_ERROR';
-export const TO_DO_LIST = '/todos';
-export const CALL_ADD_TO_DO = 'CLICK_ADD_TO_DO';
-export const ADD_TO_DO_SUCCESS = 'ADD_TO_DO_SUCCESS';
-
-// API JsonPlaceholder
-// export const TO_DO_API = 'https://jsonplaceholder.typicode.com/';
+const root = 'boilerplate/ToDos/';
+export const CALL_TODO_LIST = `${root}CALL_TODO_LIST`;
+export const CALL_TODO_SUCCESS = `${root}CALL_TODO_SUCCESS`;
+export const CALL_TODO_REJECTED = `${root}CALL_TODO_REJECTED`;
+export const DELETE_TODO = `${root}DELETE_TODO`;
+export const DELETE_TODO_ERROR = `${root}DELETE_TODO_ERROR`;
+export const CALL_ADD_TO_DO = `${root}CALL_ADD_TO_DO`;
+export const ADD_TO_DO_SUCCESS = `${root}ADD_TO_DO_SUCCESS`;
